14b407b7b54147d382f2edb307016cc5fe75897c
Arhyel Ramos Flores <arthy89>
2023-11-07 15:22:26 -0500

    master

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return 'lista de tareas';
+    }
+
+    public function create()
+    {
+        return 'crear tarea';
+        // xd
+    }
+}
